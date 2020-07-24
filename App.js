6861e425e339dc95f4c02491bcfea34b9d41706a
Fadhlaoui <fadhlaouiraied@hotmail.com>
2020-07-24 19:48:02 +0100

    MVC

--- App.js
+++ App.js
@@ -1,24 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import "antd-mobile/dist/antd-mobile.css";
-import { Button } from "antd-mobile";
-import { StyleSheet, Text, View } from "react-native";
+import Home from "./src/pages/home.jsx";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button type="primary">primary</Button>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ src/pages/home.jsx
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import "antd-mobile/dist/antd-mobile.css";
+import { Button } from "antd-mobile";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button type="primary">primary</Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
