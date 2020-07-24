f32c4e1c628795e14e7612cbe4a7c865427a88a2
Fadhlaoui <fadhlaouiraied@hotmail.com>
2020-07-24 19:38:06 +0100

    install antd + create page/component

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import "antd-mobile/dist/antd-mobile.css";
+import { Button } from "antd-mobile";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button type="primary">primary</Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
